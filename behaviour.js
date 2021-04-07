0b1995a2afe00c5760ae90ef978d069c102ca1f2
shalomalexander <shalomalexander68@gmail.com>
2021-04-07 11:22:47 +0530

    toggle menu functionality added with little animation

---
+++ behaviour.js
@@ -0,0 +1,27 @@
+function toggleFunction() {
+    var x = document.getElementById("header-nav");
+    var main = document.getElementById("main-id");
+
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      //main.style.backgroundColor = "whitesmoke";
+      main.style.opacity = "10%"
+      
+     // x.style.visibility = "visible";
+    } else {
+      x.style.display = "none";
+      main.style.backgroundColor = "";
+      main.style.opacity = ""
+    }
+  }
+
+function toggleDisplayOfNavigation() {
+    var w = window.outerWidth;
+    var x = document.getElementById("header-nav");
+    if(w>481) {
+        
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+} 
