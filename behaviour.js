efbfd86bd8ab4f49875468f136afbdd1f0167f14
shalomalexander <shalomalexander68@gmail.com>
2021-05-15 00:36:00 +0530

    Modal Sheets Added

--- behaviour.js
+++ behaviour.js
@@ -24,3 +24,53 @@ function toggleDisplayOfNavigation() {
         x.style.display = "none";
     }
 } 
+
+var modal1 = false; 
+var modal2 = false;
+var modal3 = false;
+var modal4 = false;
+
+function toggleModal(id) {
+  
+  if(id==="ehr") {
+    modal1 = !modal1;
+    var element = document.getElementById("modal-ehr");
+    if(modal1) {
+      element.classList.add("active");
+      return;
+    } 
+
+    element.classList.remove("active");
+  }
+
+  if (id==="google-meet-tracker") {
+    modal2 = !modal2;
+    var element = document.getElementById("modal-google-meet-tracker");
+    if(modal2) {
+      element.classList.add("active");
+      return;
+    } 
+    element.classList.remove("active");
+  }
+
+  if (id==="wol") {
+    modal3 = !modal3;
+    var element = document.getElementById("modal-wol");
+    if(modal3) {
+      element.classList.add("active");
+      return;
+    } 
+    element.classList.remove("active");
+  }
+
+  if (id==="meal") {
+    modal4 = !modal4;
+    var element = document.getElementById("modal-meal");
+    if(modal4) {
+      element.classList.add("active");
+      return;
+    } 
+    element.classList.remove("active");
+  }
+ 
+}
